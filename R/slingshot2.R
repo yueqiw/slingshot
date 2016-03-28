8818d4a0eb278f5cd17cb8a7a6b2471106c4e3d2
Kelly Street <kstreet@berkeley.edu>
2016-03-27 22:40:50 -0700

    changed default behavior of get_lineages() to use diagonal covariance matrix whenever the smallest cluster would return a non-invertible covariance matrix (will also give a warning)

--- R/slingshot2.R
+++ R/slingshot2.R
@@ -1,6 +1,6 @@
 # load in necessary packages
 require(princurve); require(ape); require(igraph)
-source('code/helper_functions.R')
+#source('helper_functions.R')
 
 #########################
 ### get_lineages()
@@ -23,6 +23,10 @@ source('code/helper_functions.R')
 #' @return a list with L+2 items where L is the number of lineages identified. The first L items are character vectors with the names of the clusters included in that lineage. The last two items are \code{forest}, the connectivity matrix, and \code{C}, a clusters x lineages identity matrix.
 #'
 #' @examples
+#' data("toy_data")
+#' get_lineages(X, clus.labels)
+#' get_lineages(X, clus.labels, start.clus = 'a')
+#' 
 
 get_lineages <- function(X, clus.labels, omega = Inf, start.clus = NULL, end.clus = NULL){
   # set up, remove unclustered cells (-1's)
@@ -41,17 +45,32 @@ get_lineages <- function(X, clus.labels, omega = Inf, start.clus = NULL, end.clu
     return(colMeans(x.sub))
   }))
   
-  # get distance matrix (Euclidean)
-  # D <- as.matrix(dist(centers))
-  #### define distance function between clusters (avg Mahalanobis)
-  dist.fun <- function(clus1, clus2){
-    mu1 <- colMeans(clus1)
-    mu2 <- colMeans(clus2)
-    diff <- mu1 - mu2
-    s1 <- cov(clus1)
-    s2 <- cov(clus2)
-    return(t(diff) %*% solve(s1 + s2) %*% diff)
+  # determine whether to use full covariance matrix or componentwise for distances
+  min.clus.size <- min(table(clus.labels))
+  if(min.clus.size <= ncol(X)+1){
+    # componentwise
+    message('Using diagonal covariance matrix')
+    dist.fun <- function(clus1, clus2){
+      mu1 <- colMeans(clus1)
+      mu2 <- colMeans(clus2)
+      diff <- mu1 - mu2
+      s1 <- diag(diag(cov(clus1)))
+      s2 <- diag(diag(cov(clus2)))
+      return(t(diff) %*% solve(s1 + s2) %*% diff)
+    }
+  }else{
+    # full covariance
+    dist.fun <- function(clus1, clus2){
+      mu1 <- colMeans(clus1)
+      mu2 <- colMeans(clus2)
+      diff <- mu1 - mu2
+      s1 <- cov(clus1)
+      s2 <- cov(clus2)
+      return(t(diff) %*% solve(s1 + s2) %*% diff)
+    }
   }
+
+  ### get pairwise cluster distance matrix
   D <- sapply(clusters,function(clID1){
     sapply(clusters,function(clID2){
       clus1 <- X[clus.labels == clID1,]
@@ -206,11 +225,16 @@ get_lineages <- function(X, clus.labels, omega = Inf, start.clus = NULL, end.clu
 #' @param stretch (optional)
 #' @param trace (optional)
 #' 
-#' @details 
+#' @details TODO
 #'
-#' @return 
+#' @return TODO
 #'
 #' @examples
+#' data("toy_data")
+#' lineages <- get_lineages(X, clus.labels, start.clus = 'a')
+#' get_curves(X, clus.labels, lineages)
+#' 
+
 
 get_curves <- function(X, clus.labels, lineages, thresh = 0.0001, maxit = 100, stretch = 2, trace = FALSE){
   smoother <- "smooth.spline"
@@ -359,5 +383,157 @@ get_curves <- function(X, clus.labels, lineages, thresh = 0.0001, maxit = 100, s
     pcurves[[l]]$tag <- tag[tag]
   }
   return(pcurves)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################
+### Helper functions for Slingshot
+#########################
+project_point_to_segment <- function(A,B,p){
+  AB <- B-A
+  AB_squared <- sum(AB*AB)
+  if(AB_squared==0){
+    return(A)
+  }
+  Ap <- p-A
+  t <- sum(Ap*AB)/AB_squared
+  if(t < 0){
+    return(A)
+  }
+  if(t > 1){
+    return(B)
+  }
+  return(A + t*AB)
+}
+project_points_to_segment <- function(A,B,pts){
+  if(class(pts)=='numeric'){
+    pts <- matrix(pts,ncol=length(pts))
+  }
+  n <- nrow(pts)
+  bigA <- t(matrix(A,nrow=length(A),ncol=n))
+  bigB <- t(matrix(B,nrow=length(A),ncol=n))
+  AB <- B-A
+  bigAB <- t(matrix(AB,nrow=length(A),ncol=n))
+  AB_squared <- sum(AB*AB)
+  if(AB_squared==0){
+    return(bigA)
+  }
+  bigAp <- pts-bigA
+  t <- diag(bigAp %*% t(bigAB))/AB_squared
+  final <- t(sapply(t,function(ti){
+    if(ti < 0){
+      return(A)
+    }
+    if(ti > 1){
+      return(B)
+    }
+    return(A + ti*AB)
+  }))
+  rownames(final) <- rownames(pts)
+  colnames(final) <- colnames(pts)
+  return(final[order(t),])
+}
+dist_point_to_segment <- function(A,B,p){
+  AB <- B-A
+  AB_squared <- sum(AB*AB)
+  if(AB_squared==0){
+    return(sqrt(sum((A-p)^2)))
+  }
+  Ap <- p-A
+  t <- sum(Ap*AB)/AB_squared
+  if(t < 0){
+    return(sqrt(sum((A-p)^2)))
+  }
+  if(t > 1){
+    return(sqrt(sum((B-p)^2)))
+  }
+  q <- (A + t*AB)
+  return(sqrt(sum((q-p)^2)))
+}
+project_points_to_lineage <- function(lineage,pts){
+  n <- nrow(pts)
+  K <- nrow(lineage)
+  dists <- t(apply(pts,1,function(p){
+    sapply(1:(K-1),function(k){
+      dist_point_to_segment(lineage[k,],lineage[k+1,],p)
+    })
+  }))
+  projs <- sapply(1:n, function(i){
+    k <- which.min(dists[i,])
+    p <- pts[i,]
+    return(project_point_to_segment(lineage[k,],lineage[k+1,], p))
+  })
+  return(t(projs))
+}
+dist_points_to_lineage <- function(lineage,pts){
+  d <- apply(pts,1,function(p){
+    K <- nrow(lineage)
+    min(sapply(1:(K-1),function(k){
+      dist_point_to_segment(lineage[k,],lineage[k+1,],p)
+    }))
+  })
+  return(d)
+}
+lineage_length <- function(lineage){
+  if(class(lineage)=="numeric"){
+    return(0)
+  }
+  K <- nrow(lineage)
+  d <- 0
+  for(i in 1:(K-1)){
+    d <- d + sqrt(sum((lineage[i,]-lineage[i+1,])^2))
+  }
+  return(d)
+}
+get_connections <- function(clus, forest, parent = NULL){
+  children.idx <- forest[,clus] == 1
+  children <- rownames(forest)[children.idx]
+  if(is.null(parent)){
+    out <- clus
+    for(child in children){
+      out <- c(out, Recall(child, forest, clus))
+    }
+  }else{
+    children <- children[children != parent]
+    out <- clus
+    for(child in children){
+      out <- c(out, Recall(child, forest, clus))
+    }
+  }
+  return(out)
+}
+scale01 <- function(x){
+  return((x - min(x,na.rm=T))/max(x - min(x,na.rm=T)))
+}
+avg.curves <- function(pcurves){
+  p <- ncol(pcurves[[1]]$s)
+  lambdas.all <- lapply(pcurves, function(pcv){pcv$lambda})
+  lambdas.all <- unique(unlist(lambdas.all))
+  max.shared.lambda <- min(sapply(pcurves, function(pcv){max(pcv$lambda)}))
+  lambdas.all <- sort(lambdas.all[lambdas.all <= max.shared.lambda])
+  pcurves.dense <- lapply(pcurves,function(pcv){
+    sapply(1:p,function(jj){
+      interpolated <- approx(pcv$lambda, pcv$s[,jj], xout = lambdas.all)$y
+      return(interpolated)
+    })
+  })
+  avg <- sapply(1:p,function(jj){
+    dim.all <- sapply(1:length(pcurves.dense),function(i){ pcurves.dense[[i]][,jj] })
+    return(rowMeans(dim.all))
+  })
+  ind <- !duplicated(lambdas.all)
+  return(list(avg=avg[ind,],lambda=lambdas.all[ind]))
 }
 

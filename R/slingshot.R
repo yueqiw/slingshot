f4d1821d49ae828f5e0a98c1b8bd1df8fcd4ea6b
Kelly Street <kstreet@berkeley.edu>
2016-05-04 16:17:50 -0700

    changed the structure. get_lineages and get_curves are their own scripts now, removed the '2', added AllGenerics and AllClasses, renamed helper functions AllHelper

---
+++ R/slingshot.R
@@ -0,0 +1,19 @@
+# the BIG wrapper function
+
+#' @title Identify transcriptional trajectories and order cells
+#' 
+#' @description Performs all parts of a single-cell RNA-Seq analysis with continuous differentiation processes.
+#' 
+#' @param X numeric, the nxp matrix of samples
+#' 
+#' @details TODO
+#'
+#' @return TODO
+#'
+#' @examples
+#' data("toy_data")
+#' 
+# @export
+#' 
+
+#slingshot <- function(X){}

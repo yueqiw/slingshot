6a5aa4373c289d0a4a85ee20b621082ac5c860e4
Kelly Street <kstreet@berkeley.edu>
2016-05-09 13:12:51 -0700

    added files to be used for wrapper functions for normalization, dimensionality reduction, and clustering as well as generics for these.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -5,6 +5,24 @@ setGeneric(
   }
 )
 setGeneric(
+  name = "get_normalization",
+  def = function(expr,  ...) {
+    standardGeneric("get_normalization")
+  }
+)
+setGeneric(
+  name = "get_reducedDim",
+  def = function(expr,  ...) {
+    standardGeneric("get_reducedDim")
+  }
+)
+setGeneric(
+  name = "get_clusters",
+  def = function(x,  ...) {
+    standardGeneric("get_clusters")
+  }
+)
+setGeneric(
   name = "get_clustMap",
   def = function(x,  ...) {
     standardGeneric("get_clustMap")
